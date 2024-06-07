2d94e8119cc3bb95db12756944af370d0c761909
Keshav <93115745+keshavkumar143@users.noreply.github.com>
2024-06-07 18:09:53 +0530

    Add files via upload

---
+++ ques6.java
@@ -0,0 +1,62 @@
+// Definition for a binary tree node.
+//BALANCED BINARY TREE 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    
+    TreeNode() {}
+    
+    TreeNode(int val) { 
+        this.val = val; 
+    }
+    
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    boolean flag = true; 
+
+    public int num(TreeNode root) { 
+        if (root == null || !flag) { 
+            return 0; 
+        }
+
+        int a = num(root.right);
+        int b = num(root.left); 
+        int k = Math.abs(a - b); 
+
+        if (k > 1) {
+            flag = false; 
+        }
+
+        return Math.max(a + 1, b + 1); 
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        num(root); 
+        return flag; 
+    }
+}
+
+public class ques6 {
+    public static void main(String[] args) {
+        // Create a test case for the binary tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        root.left.left.left = new TreeNode(7);
+
+        Solution solution = new Solution();
+        boolean result = solution.isBalanced(root);
+
+        System.out.println(result);
+    }
+}
