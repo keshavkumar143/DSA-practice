358507fc0bb1175c20566cc4d87abcf79e3092e9
Keshav <93115745+keshavkumar143@users.noreply.github.com>
2024-06-12 10:43:19 +0530

    Add files via upload

---
+++ ques8.java
@@ -0,0 +1,51 @@
+
+//MAXIMUM DEAPTH OF BINARY TREE 
+//LEETCODE LINK --> https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class Depth {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
+
+
+public class ques8 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        Depth sol = new Depth();
+        int depth = sol.maxDepth(root);
+        System.out.println("Maximum depth of the binary tree is: " + depth);
+    }
+}
+
