2ad5d76f3fd29416e69e7c6cee8149d63b9ae7ac
Keshav <93115745+keshavkumar143@users.noreply.github.com>
2024-08-07 01:02:22 +0530

    Add files via upload

---
+++ ques20.java
@@ -0,0 +1,23 @@
+import java.util.*; 
+//Tower of Hanoi
+//Time complexcity: O(2n-1) || O(2n)
+public class ques20 {
+    public static void towerOfHanoi(int n, String source, String helper, String Destination){
+
+        if(n==1){
+           System.out.println("transfer disk " + n + " from " + source + " to " +Destination);
+          return; 
+        }
+          
+        towerOfHanoi(n-1, source, helper, helper);
+        System.out.println("transfer disk " + n + " from " + source + " to " +Destination); 
+    
+        towerOfHanoi(n-1, helper, source, Destination);
+      }
+      
+      
+      public static void main(String[] args) {
+        int n = 2; 
+        towerOfHanoi(n, "S", "H", "D");
+      }
+}
